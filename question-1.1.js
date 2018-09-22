416f7e2cc279be0a1c0c3fece394ea1951ce9485
leekevinyg <klee2010@gmail.com>
2018-09-22 11:50:59 -0700

    indent using spaces

--- question-1.1.js
+++ question-1.1.js
@@ -1,21 +1,21 @@
 /* 
 
-1.1 
+Question 1.1 
 
 Implement an function to determine if a string has all unique characters
 
 */
 
 const allUnique = (string) => {
-	let charactersEncounteredSoFar = [];
-	let stringArray = string.split();
-	stringArray.forEach((character) => {
-		if (!charactersEncounteredSoFar[character]) {
-			charactersEncounteredSoFar[character] = true;
-		}
-		return false;
-	});
-	return true;
+    let charactersEncounteredSoFar = [];
+    let stringArray = string.split();
+    stringArray.forEach((character) => {
+	   if (!charactersEncounteredSoFar[character]) {
+		  charactersEncounteredSoFar[character] = true;
+        }
+        return false;
+    });
+    return true;
 }
 
 module.exports = allUnique;
